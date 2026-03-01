60bf8749a233e1fca6e051bc19eadc9729d7de17
aditya singh <aditya.23cse362@rtu.ac.in>
2026-03-01 19:17:54 +0530

    Mail integration

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -4,11 +4,13 @@ import { PrismaClient } from '../generated/prisma/client'
 const globalForPrisma = globalThis
 
 const connectionString = process.env.DATABASE_URL
+
 if (!connectionString) {
-  throw new Error('DATABASE_URL environment variable is not set')
+  throw new Error('DATABASE_URL is not set')
 }
 
 const adapter = new PrismaPg({ connectionString })
+
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
